fdacb5a84879cbafe9dcdbcf2f9e62c6928f4f5a
ferlicjl <ferlicjl@gmail.com>
2019-06-24 08:55:32 -0400

    added approx to easy functions

--- R/easyFunctions.R
+++ R/easyFunctions.R
@@ -5,18 +5,19 @@
 #' @param birth constant birth rate
 #' @param init initial population size
 #' @param time number of time units to simulate
+#' @param approx boolean if TRUE, approximate simulation is used, if FALSE, exact simulation is performed
 #'
 #' @export
 #' @examples
 #' \dontrun{
 #' simBirth(birth = 1, init = 100, time = 10)
 #' }
-simBirth = function(birth = 1.0, init = 100, time = 1){
+simBirth = function(birth = 1.0, init = 100, time = 1, approx = FALSE){
   transitionList = TransitionList(FixedTransition(population = 0, rate = birth, fixed = c(2)))
 
   stopList = StopList()
 
-  return(estipop:::branch(time, init, transitionList, stopList, keep = FALSE, silent = TRUE))
+  return(estipop:::branch(time, init, transitionList, stopList, keep = FALSE, silent = TRUE, approx))
 }
 
 #' simBirthDeath
@@ -27,19 +28,20 @@ simBirth = function(birth = 1.0, init = 100, time = 1){
 #' @param death constant death rate
 #' @param init initial population size
 #' @param time number of time units to simulate
+#' @param approx boolean if TRUE, approximate simulation is used, if FALSE, exact simulation is performed
 #'
 #' @export
 #' @examples
 #' \dontrun{
 #' simBirthDeath(birth = 1, death = 0.7, init = 100, time = 10)
 #' }
-simBirthDeath = function(birth = 1.0, death = 0.0, init = 100, time = 1){
+simBirthDeath = function(birth = 1.0, death = 0.0, init = 100, time = 1, approx = FALSE){
   transitionList = TransitionList(FixedTransition(population = 0, rate = birth, fixed = c(2)),
                                   FixedTransition(population = 0, rate = death, fixed = c(0)))
 
   stopList = StopList()
 
-  return(estipop:::branch(time, init, transitionList, stopList, keep = FALSE, silent = TRUE))
+  return(estipop:::branch(time, init, transitionList, stopList, keep = FALSE, silent = TRUE, approx))
 }
 
 #' simpBirthDeath
@@ -79,20 +81,21 @@ simpBirthDeath = function(b = 1.0, d = 0.0, init = 100, t = 1, N = 1){
 #' @param birth2 constant birth rate of population 2
 #' @param init vector of initial population sizes
 #' @param time number of time units to simulate
+#' @param approx boolean if TRUE, approximate simulation is used, if FALSE, exact simulation is performed
 #'
 #' @export
 #' @examples
 #' \dontrun{
 #' simBirthMutation(birth1 = 1, death1 = 0.7, mutation = 0.1, birth2 = 1.0, death2 = 0.7, init = c(100,0), time = 10)
 #' }
-simBirthMutation = function(birth1 = 1.0, mutation = 0.0, birth2 = 1.0, init = c(100,0), time = 1){
+simBirthMutation = function(birth1 = 1.0, mutation = 0.0, birth2 = 1.0, init = c(100,0), time = 1, approx = FALSE){
   transitionList = TransitionList(FixedTransition(population = 0, rate = birth1, fixed = c(2,0)),
                                   FixedTransition(population = 0, rate = mutation, fixed = c(1,1)),
                                   FixedTransition(population = 1, rate = birth2, fixed = c(0,2)))
 
   stopList = StopList()
 
-  return(estipop:::branch(time, init, transitionList, stopList, keep = FALSE, silent = TRUE))
+  return(estipop:::branch(time, init, transitionList, stopList, keep = FALSE, silent = TRUE, approx))
 }
 
 #' simBirthDeathMutation
@@ -106,13 +109,14 @@ simBirthMutation = function(birth1 = 1.0, mutation = 0.0, birth2 = 1.0, init = c
 #' @param death2 constant death rate of population 2
 #' @param init vector of initial population sizes
 #' @param time number of time units to simulate
+#' @param approx boolean if TRUE, approximate simulation is used, if FALSE, exact simulation is performed
 #'
 #' @export
 #' @examples
 #' \dontrun{
 #' simBirthDeathMutation(birth1 = 1, death1 = 0.7, mutation = 0.1, birth2 = 1.0, death2 = 0.7, init = c(100,0), time = 10)
 #' }
-simBirthDeathMutation = function(birth1 = 1.0, death1 = 0.0, mutation = 0.0, birth2 = 1.0, death2 = 0.7, init = c(100,0), time = 1){
+simBirthDeathMutation = function(birth1 = 1.0, death1 = 0.0, mutation = 0.0, birth2 = 1.0, death2 = 0.7, init = c(100,0), time = 1, approx = FALSE){
   transitionList = TransitionList(FixedTransition(population = 0, rate = birth1, fixed = c(2,0)),
                                   FixedTransition(population = 0, rate = death1, fixed = c(0,0)),
                                   FixedTransition(population = 0, rate = mutation, fixed = c(1,1)),
@@ -121,7 +125,7 @@ simBirthDeathMutation = function(birth1 = 1.0, death1 = 0.0, mutation = 0.0, bir
 
   stopList = StopList()
 
-  return(estipop:::branch(time, init, transitionList, stopList, keep = FALSE, silent = TRUE))
+  return(estipop:::branch(time, init, transitionList, stopList, keep = FALSE, silent = TRUE, approx))
 }
 
 #' simTwoTypeResistance
@@ -142,6 +146,7 @@ simBirthDeathMutation = function(birth1 = 1.0, death1 = 0.0, mutation = 0.0, bir
 #' @param death12 constant death rate of population 1/2
 #' @param init vector of initial population sizes
 #' @param time number of time units to simulate
+#' @param approx boolean if TRUE, approximate simulation is used, if FALSE, exact simulation is performed
 #'
 #' @export
 #' @examples
@@ -153,7 +158,7 @@ simTwoTypeResistance = function(mutation1 = 0.0, mutation2 = 0.0, mutation12 = 0
                                 birth1 = 1.0, death1 = 0.7,
                                 birth2 = 1.0, death2 = 0.7,
                                 birth12 = 1.0, death12 = 0.7,
-                                init = c(100,0,0,0), time = 1){
+                                init = c(100,0,0,0), time = 1, approx = FALSE){
   transitionList = TransitionList(FixedTransition(population = 0, rate = mutation1, fixed = c(1, 1, 0, 0)),
                                   FixedTransition(population = 0, rate = mutation2, fixed = c(1, 0, 1, 0)),
                                   FixedTransition(population = 1, rate = mutation12, fixed = c(0, 1, 0, 1)),
@@ -169,6 +174,6 @@ simTwoTypeResistance = function(mutation1 = 0.0, mutation2 = 0.0, mutation12 = 0
 
   stopList = StopList()
 
-  return(estipop:::branch(time, init, transitionList, stopList, keep = FALSE, silent = TRUE))
+  return(estipop:::branch(time, init, transitionList, stopList, keep = FALSE, silent = TRUE, approx))
 }
 
